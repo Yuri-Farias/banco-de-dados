b60f7d02940ddf4b0743bef9c4c38771d2f1b57d
Yuri 7 <yurifarias444@gmail.com>
2024-07-26 10:28:39 -0300

    Add files via upload

---
+++ insert-into.sql
@@ -0,0 +1,20 @@
+insert into `db_senac`.`tbl_pessoas` (
+    `nome`,
+    `rg`,
+    `cpf`,
+    `data_nascimento`,
+    `email`,
+    `whatsapp`,
+    `senha`,
+    `genero`,
+    `naturalidade`
+) values (
+    '123456789-ADR-AM',
+    '321.654.987-01',
+    '32/13/-2056',
+    'login@provedor.com.seila',
+    '+965 (49)165289-*56239',
+    '123456',
+    'masculino',
+    'goiás / mato grosso do sul'
+);
